93eb096e97cd6b5087000e126887cb3893255cd0
Isini-Dananjana <isinidan97@gmail.com>
2021-07-15 12:03:07 +0530

    script upload function implemented

--- src/api/log_api.go
+++ src/api/log_api.go
@@ -152,14 +152,10 @@ func HandleFileUpdates(w http.ResponseWriter, r *http.Request) {
 func HandleSrciptUpload(w http.ResponseWriter, r *http.Request){
 
 	fmt.Println("File Upload Endpoint Hit")
-
-    // Parse our multipart form, 10 << 20 specifies a maximum
-    // upload of 10 MB files.
     r.ParseMultipartForm(10 << 20)
-    // FormFile returns the first file for the given key `myFile`
-    // it also returns the FileHeader so we can get the Filename,
-    // the Header and the size of the file
+   
     file, handler, err := r.FormFile("myFile")
+	fileId:=r.FormValue("fileId")
     if err != nil {
         fmt.Println("Error Retrieving the File")
         fmt.Println(err)
@@ -170,24 +166,17 @@ func HandleSrciptUpload(w http.ResponseWriter, r *http.Request){
     fmt.Printf("File Size: %+v\n", handler.Size)
     fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-    // Create a temporary file within our temp-images directory that follows
-    // a particular naming pattern
-    tempFile, err := ioutil.TempFile("localstorage", "*.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer tempFile.Close()
-
-    // read all of the contents of our uploaded file into a
     // byte array
     fileBytes, err := ioutil.ReadAll(file)
     if err != nil {
         fmt.Println(err)
     }
-    // write this byte array to our temporary file
-    tempFile.Write(fileBytes)
-    // return that we have successfully uploaded our file!
-    fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	 err = ioutil.WriteFile("localstorage/"+fileId+"/script.txt",  fileBytes, 0777)
+	 if err != nil {	
+		 fmt.Println(err)
+	 }
+   fmt.Fprintf(w, "Successfully Uploaded File\n")
 
 }
 
